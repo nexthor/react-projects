b1c694655396208d616d6092e7dfc5f02038ab4f
Nestor Danilo Mendoza <nestordanilo.mendoza@concentrix.com>
2024-11-14 18:53:22 -0600

    i must to finish chapter-2

--- chapter-2/src/App.js
+++ chapter-2/src/App.js
@@ -1,11 +1,15 @@
+import React from 'react';
 import logo from './assets/logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <div className="App">
       <Header logo={logo} />
+      <Profile userName="octocat" />
+      <Profile userName="nexthor" />
     </div>
   );
 }
--- chapter-2/src/components/Link.js
+++ chapter-2/src/components/Link.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './Link.css';
 
 function Link({ url, title }) {
